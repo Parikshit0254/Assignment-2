81d73a913516d9c0b2a0544cc9654279d96a6d18
Parikshit0254 <parikshitsharma901@gmail.com>
2025-07-06 13:34:44 +0530

    Add files via upload

---
+++ Assignment 2 task 2.py
@@ -0,0 +1,9 @@
+for i in range(1,51) :
+   print(i)
+a=sum(range(1,51))
+print('The sum of the numbers are =' ,a)
+
+
+
+
+
---
+++ assignment 2 task 1.py
@@ -0,0 +1,7 @@
+a=int(input('enter your number= '))
+
+if (a/2==int):
+    print('The number is even')
+    
+else:
+    print('The number is odd')
